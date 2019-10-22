61213581c4b6da3839b5a70a79e67262f18fdccd
alexluckyq <alex.qyq@gmail.com>
2019-10-22 00:33:00 -0400

    Adding search fragment and search adapters

---
+++ app/src/main/java/bell/assignment/simpletwitterclient/adapters/SearchRecyclerViewAdapter.kt
@@ -0,0 +1,25 @@
+package bell.assignment.simpletwitterclient.adapters
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import bell.assignment.simpletwitterclient.databinding.LayoutSearchListitemBinding
+
+class SearchRecyclerViewAdapter() : RecyclerView.Adapter<SearchRecyclerViewAdapter.SearchViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class SearchViewHolder(val binding: LayoutSearchListitemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+    }
+}
--- app/src/main/java/bell/assignment/simpletwitterclient/fragments/SearchFragment.kt
+++ app/src/main/java/bell/assignment/simpletwitterclient/fragments/SearchFragment.kt
@@ -1,6 +1,29 @@
 package bell.assignment.simpletwitterclient.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import bell.assignment.simpletwitterclient.adapters.SearchRecyclerViewAdapter
+import bell.assignment.simpletwitterclient.viewmodels.SearchViewModel
+
 
 class SearchFragment: Fragment() {
+
+    private lateinit var searchViewModel: SearchViewModel
+    private lateinit var searchAdapter: SearchRecyclerViewAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        searchViewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
+        val view = inflater.inflate(bell.assignment.simpletwitterclient.R.layout.fragment_search, container, false)
+        return view
+    }
+
+
 }
